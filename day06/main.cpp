323839512e999232fa5ecb7828cc43e026ef5484
Christoph Haack <christoph.haack@tuhh.de>
2022-12-06 17:26:57 +0100

    T_06

--- day06/main.cpp
+++ day06/main.cpp
@@ -8,23 +8,26 @@ using namespace std;
 
 
 
-int main(){
-    fstream inputfile;
-    inputfile.open("input.txt",ios::in);
-    string input_string;
-    getline(inputfile, input_string);
-    cout << input_string << "\n";
+int main(){                                     //
+    fstream inputfile;                          //  creating of the file stream
+    inputfile.open("input.txt",ios::in);        //  standard file opening and reading
+    string input_string;                        //  string to be copied into
+    getline(inputfile, input_string);           //  all the content is in one line, which is copied
+    cout << input_string << "\n";               //  printing of content
 
-    int char_pos;
+    int char_pos;       //the int value, that signifies the position of the requested character
 
-    for(int i = 0; i <= input_string.length(); i++){
+    for(int i = 0; i <= input_string.length(); i++){        //iterates through each character, with the iterator i
         if( ( input_string.at(i) != input_string.at(i +1) ) && ( input_string.at(i) != input_string.at(i +2) ) && ( input_string.at(i) != input_string.at(i +3) ) ){
+            //tests if the first character is distinctive compared to the second, third and fourth one
             if( ( input_string.at(i +1) != input_string.at(i +2) ) && ( input_string.at(i +1) != input_string.at(i +3) ) ){
+                //tests if the second one is distinctive compared to the third and fourth one
                 if( input_string.at(i +2) != input_string.at(i +3)){
-                    char_pos = i +4;
-                    cout << input_string.at(i) << input_string.at(i +1) << input_string.at(i +2) <<  input_string.at(i +3) << "\n";
-                    cout << char_pos << "\n";
-                    i = input_string.length();
+                    //tests if the third one is distinctive to the fourth one
+                    char_pos = i +4;    //setting the requested position of the char
+                    cout << input_string.at(i) << input_string.at(i +1) << input_string.at(i +2) <<  input_string.at(i +3) << "\n"; //printing the compared chars
+                    cout << char_pos << "\n";   //printing the requested positional value
+                    i = input_string.length();  //breaking the cycle, by setting the iterator directly to the end
                 }
             }
         }        
--- day06/main2.cpp
+++ day06/main2.cpp
@@ -10,31 +10,28 @@ using namespace std;
 
 
 int main(){
-    fstream inputfile;
-    inputfile.open("input.txt",ios::in);
-    string input_string, sorted_string_segment;
-    getline(inputfile, input_string);
-    cout << input_string << "\n";
+    fstream inputfile;                              //creating file stream
+    inputfile.open("input.txt",ios::in);            //opening file in filestream
+    string input_string, sorted_string_segment;     //defining the strings
+    getline(inputfile, input_string);               //copying the content into the string
+    cout << input_string << "\n";                   //printing content
+    int char_pos, counter;                          //defining the ints
 
-    int char_pos, counter;
-    
-
-    for(int i = 0; i <= input_string.length(); i++){
-        sorted_string_segment = input_string.substr(i, 14);
-        sort(sorted_string_segment.begin(), sorted_string_segment.end());
-        for(int k = 0; k <= 12; k++){
-            if(sorted_string_segment.at(k) != sorted_string_segment.at(k +1)){
-                counter = counter +1;
+    for(int i = 0; i <= input_string.length(); i++){        //iterating through each character with the iterator i
+        sorted_string_segment = input_string.substr(i, 14);     //copying the content of the next 14 chars of the string into a temp_string
+        sort(sorted_string_segment.begin(), sorted_string_segment.end());   //sorting the temp_string
+        for(int k = 0; k <= 12; k++){       //iterating through the the temp_string for each comparison (13)
+            if(sorted_string_segment.at(k) != sorted_string_segment.at(k +1)){      //testing if the current elememt of the sorted string is distinctive compared to the next one
+                counter = counter +1;   //in that case adding one to the temp counter
             }
         }
-        if(counter == 13){
-            char_pos = i +14;
-            i = input_string.length();
-            cout << char_pos << "\n";
+        if(counter == 13){      //checking if every character of the 14 chars in the temp sorted string were distinctive
+            char_pos = i +14;       //setting the requested char pos value
+            i = input_string.length();  //breaking the loop, by setting the iterator i to the end
+            cout << char_pos << "\n";   //printing the requested value
         }
-        else{
-            counter = 0;
+        else{       //in case the chrs were not distinctive
+            counter = 0;    //reseting counter
         }
     }
-
 }
